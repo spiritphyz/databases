b0414a17e5bfcd72d435c56857614d8d79953772
spiritphyz <heytonyle@icloud.com>
2016-10-07 22:16:30 -0700

    Access the POST body using express

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,4 @@
 var models = require('../models');
-var fs = require('fs');
-
 
 var headers = {
   'access-control-allow-origin': '*',
@@ -15,21 +13,7 @@ module.exports = {
   messages: {
     get: function (req, res) {}, // a function which handles a get request for all messages
     post: function (req, res) {
-      content = '';
-      req.on('data', function(chunk) {
-        content += chunk;
-      });
-      req.on('end', function() {
-        content = content.toString();
-        fs.appendFile('/Volumes/student/2016-09-databases/server/fakeMessages.txt', content, 'utf8', function(err) {
-          if (err) {
-            console.log('error on appendFile', err);
-          }
-        });
-      });
-      console.log('content inside post is', content);
-      res.writeHead(200, headers);
-      res.end();
+      console.log('body from user POST is: ', req.body);
     } // a function which handles posting a message to the database
   },
 
