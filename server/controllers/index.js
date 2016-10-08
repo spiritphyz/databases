df6ff1b48596c9bd9ab6cf04b610977c4c0794e4
spiritphyz <heytonyle@icloud.com>
2016-10-08 16:24:33 -0700

    Implement timestamp for 'CreatedAt' field
    - Created new field in messages table with 'ALTER' command
    - Used mysql NOW() to get proper timestamp during POST

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -9,18 +9,18 @@ module.exports = {
     get: function (req, res) { // a function which handles a get request for all messages
       var messages = new Promise(function(todo, todont) {
           sql.query('select * from messages order by id desc limit 40;', function(err, results, fields) {
-          console.log('getting messages from db', results);
+          console.log('getting messages from db');
           return err ? todont(err) : todo(results);
         });
       }).then(function(results) {
-          console.log('here are results from db', results);
+          console.log('here are results from db');
           var properResults = [].concat(results.reduce((acc, cur) => { 
             return acc.concat( { 
               text: cur.text,
               username: 'zippy',
               roomname: 'lobby',
-              createdAt: '',
-              updatedAt: '',
+              createdAt: cur.createdAt,
+              updatedAt: cur.createdAt,
               objectId: cur.objectId
             } );
           }, []));
@@ -34,7 +34,7 @@ module.exports = {
 
       var user = new Promise(function(todo, todont) {
         var newid = Math.floor(Math.random()* 1292665987).toString(36); 
-          sql.query('INSERT INTO `messages` (`id`, `text`, `user_id`, `room_id`, `objectId` ) VALUES ( NULL, ' + mysql.escape(req.body.text) + ', \'1\', \'1\', \'' + newid + '\');', function(err, results, fields) {
+          sql.query('INSERT INTO `messages` (`id`, `text`, `user_id`, `room_id`, `objectId`, `createdAt` ) VALUES ( NULL, ' + mysql.escape(req.body.text) + ', \'1\', \'1\', \'' + newid + '\', NOW());', function(err, results, fields) {
           console.log('inserted new data into database');
           return err ? todont(err) : todo(results);
           } );
